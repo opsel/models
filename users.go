6763ac7114554b10381145029c980bef280bb48b
shyaminayesh <me@shyamin.com>
2023-07-04 16:23:40 +0530

    models

---
+++ users.go
@@ -0,0 +1,16 @@
+package models
+
+type Users struct {
+	ID        uint64                `gorm:"column:id;primaryKey"`
+	FirstName string                `gorm:"column:name_first"`
+	LastName  string                `gorm:"column:name_last"`
+	Email     string                `gorm:"column:email"`
+	Password  string                `gorm:"column:password"`
+	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
+	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null"`
+}
+
+func (Users) TableName() string {
+	return "users"
+}
