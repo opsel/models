07b2cb774243e8f3a3192a472dff767cbeb0c4ba
shyaminayesh <me@shyamin.com>
2023-07-04 17:30:56 +0530

    optimize field tags

--- agent.go
+++ agent.go
@@ -3,10 +3,10 @@ package models
 import "gorm.io/plugin/soft_delete"
 
 type Agent struct {
-	ID        uint64                `gorm:"column:id;primaryKey"`
-	UUID      string                `gorm:"column:uuid"`
-	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
+	ID        uint64                `gorm:"column:id;primaryKey;autoIncrement"`
+	UUID      string                `gorm:"column:uuid;unique"`
+	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime;default:null"`
+	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime;default:null"`
 	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null;index"`
 }
 
--- users.go
+++ users.go
@@ -3,13 +3,13 @@ package models
 import "gorm.io/plugin/soft_delete"
 
 type Users struct {
-	ID        uint64                `gorm:"column:id;primaryKey"`
+	ID        uint64                `gorm:"column:id;primaryKey;autoIncrement"`
 	FirstName string                `gorm:"column:name_first"`
 	LastName  string                `gorm:"column:name_last"`
-	Email     string                `gorm:"column:email"`
+	Email     string                `gorm:"column:email;unique"`
 	Password  string                `gorm:"column:password"`
-	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
+	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime;default:null"`
+	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime;default:null"`
 	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null;index"`
 }
 
